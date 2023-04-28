fcc7f95a4e679e88053d7b0c4eb6a67ed0a7b9a7
DarshanaVenkatesh <venkat82@purdue.edu>
2023-04-28 03:06:30 -0400

    removed trailing slashes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(
 
 const cors = require('cors')
 const corsOptions ={
-    origin:['https://moodpalette-api.onrender.com/', "https://moodpalette.onrender.com/"],
+    origin:['https://moodpalette-api.onrender.com', 'https://moodpalette.onrender.com'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
