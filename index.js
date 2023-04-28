9dda2c33459810b8059872ab1c6050662e5d223c
DarshanaVenkatesh <venkat82@purdue.edu>
2023-04-28 02:21:54 -0400

    added express.static

--- index.js
+++ index.js
@@ -24,16 +24,18 @@ mongoose.connect(
 );
 
 const cors = require('cors')
+/*
 const corsOptions ={
     origin:['http://localhost:8800', 'https://moodpalette-api.onrender.com/'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
-
+*/
 
 //middleware
-app.use(express.json());
+//app.use(express.json());
+app.use(express.static());
 app.use(helmet());
 app.use(morgan("common"));
 
