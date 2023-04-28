3f0fe6e286c7c47e2a5746996b5a89ecc9e4f0cb
DarshanaVenkatesh <venkat82@purdue.edu>
2023-04-28 02:32:14 -0400

    removed cors

--- index.js
+++ index.js
@@ -24,14 +24,13 @@ mongoose.connect(
 );
 
 const cors = require('cors')
-/*
-const corsOptions ={
-    origin:['http://localhost:8800', 'https://moodpalette-api.onrender.com/'],
+
+/*const corsOptions ={
+    origin:['https://moodpalette-api.onrender.com/', ""],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
-*/
+app.use(cors(corsOptions));*/
 
 //middleware
 //app.use(express.json());
